9b204737501fc78527b9c142ba48e4128e79233c
k-a-r-l-o <162566291+k-a-r-l-o@users.noreply.github.com>
2024-05-17 21:32:35 +0800

    Update indexAdmin.php

--- indexAdmin.php
+++ indexAdmin.php
@@ -34,7 +34,7 @@ if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["user
             // Password is correct and usertype matches, set session and redirect
             $_SESSION["username"] = $input_username;
             $_SESSION["usertype"] = $input_usertype;
-            header("Location: Dashboard.html");
+            header("Location: Dashboard.php");
             exit();
         } else {
             // Password or usertype is incorrect
