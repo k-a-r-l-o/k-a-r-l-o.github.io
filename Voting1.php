2b499a62c896b10822d90369023ea84e1ed1003c
k-a-r-l-o <162566291+k-a-r-l-o@users.noreply.github.com>
2024-06-03 05:51:40 +0800

    Update Voting1.php

--- Voting1.php
+++ Voting1.php
@@ -572,7 +572,7 @@ $usep_ID = $_SESSION["usep_ID"];
             <div class="cardcontainer">
                 <div class="title">
                     <div class="tname">
-                        <h2>STUDENT COUNCIL</h2>
+                        <h2>TAGUM STUDENT COUNCIL</h2>
                     </div>
                 </div>
                 <?php
@@ -817,58 +817,6 @@ $usep_ID = $_SESSION["usep_ID"];
             });';
         }
         ?>
-
-        // Function to set a cookie
-        function setCookie(cookieName, cookieValue, expirationDays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (expirationHours * 60 * 60 * 1000));
-            var expires = "expires=" + d.toUTCString();
-            document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-        }
-
-        // Function to get the value of a cookie
-        function getCookie(cookieName) {
-            var name = cookieName + "=";
-            var decodedCookie = decodeURIComponent(document.cookie);
-            var cookieArray = decodedCookie.split(';');
-            for (var i = 0; i < cookieArray.length; i++) {
-                var cookie = cookieArray[i];
-                while (cookie.charAt(0) === ' ') {
-                    cookie = cookie.substring(1);
-                }
-                if (cookie.indexOf(name) === 0) {
-                    var cookieValue = cookie.substring(name.length, cookie.length);
-                    // Check if the cookie value is an image URL or base64 data
-                    if (isValidImageUrl(cookieValue)) {
-                        // If it's a URL, you can directly use it
-                        return cookieValue;
-                    } else {
-                        // If it's base64 data, you can create an image element and set its src
-                        var img = new Image();
-                        img.src = cookieValue;
-                        return img;
-                    }
-                }
-            }
-            return "";
-        }
-
-        // Event listener for form submission
-        document.getElementById("votingForm").addEventListener("submit", function(event) {
-            // Get the selected candidate value
-            var selectedCandidate = document.querySelector('input[name="position"]:checked').value;
-
-            // Set the cookie with the selected candidate value
-            setCookie("selectedCandidate", selectedCandidate, 1); // Set cookie to expire in 1 day
-        });
-
-        // Function to restore selected radio button based on cookie
-        window.onload = function() {
-            var selectedCandidate = getCookie("selectedCandidate");
-            if (selectedCandidate !== "") {
-                document.querySelector('input[name="position"][value="' + selectedCandidate + '"]').checked = true;
-            }
-        };
     </script>
 
 </body>
