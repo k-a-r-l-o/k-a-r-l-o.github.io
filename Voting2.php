e40073ec56419636c1a68288de2d794d6dbe5f98
k-a-r-l-o <162566291+k-a-r-l-o@users.noreply.github.com>
2024-06-02 22:18:17 +0800

    Update Voting2.php

--- Voting2.php
+++ Voting2.php
@@ -620,7 +620,7 @@ $usep_ID = $_SESSION["usep_ID"];
                 </div>
 
                 <div class="button">
-                    <button id="customBackButton">Back</button>
+                    <button onclick="switchHTML('Voting1.php')">Back</button>
                     <button type="submit" name="submit">Submit</button>
                 </div>
                 </form>
