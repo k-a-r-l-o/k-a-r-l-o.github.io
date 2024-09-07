cd19e9d078a0fa7020e57de643d4dcd33028dbcf
k-a-r-l-o <162566291+k-a-r-l-o@users.noreply.github.com>
2024-09-07 11:29:46 +0800

    Update sort_cand.php

--- sort_cand.php
+++ sort_cand.php
@@ -63,7 +63,7 @@ if ($result->num_rows > 0) {
 <?php
     }
 } else {
-    echo "<tr><td colspan='5'>No candidates found.</td></tr>";
+    echo "<tr>class='tdfirst'></td><td colspan='3'>No candidates found.</td><td class='tdlast'></td></tr>";
 }
 
 ?>
